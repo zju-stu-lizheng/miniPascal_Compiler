d841dad28f6cf19567eac67379b7f06fb89ca38d
zjg-stu-lizheng <3557727504@qq.com>
2022-05-12 11:25:21 +0800

    debug for ast & add comment format "//"

--- src/ast/AST_BaseNode.hpp
+++ src/ast/AST_BaseNode.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,7 +9,7 @@ class AST_BaseNode{
         int column;
         int row;
     public:
-        AST_BaseNode(); //default
+        AST_BaseNode() = default; //default
         AST_BaseNode(int _column, int _row):column(_column), row(_row){}
         int GetColumn() const{
             return this->column;
