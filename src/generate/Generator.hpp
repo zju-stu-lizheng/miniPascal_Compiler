085a7974d4e6d055a05cf2673896b30e69aba383
Alice-6161 <3190102199@zju.edu.cn>
2022-05-14 08:34:07 +0000

    add generator

---
+++ src/generate/Generator.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Value.h>
+#include "../type/type.hpp"
+#include <map>
+
+
+namespace BaseContents{
+    class CodeBlock{
+
+    };
+    int num_of_global_variables = 0;
+    int num_of_temp_variables = 0;
+    
+    llvm::LLVMContext context;
+    llvm::IRBuilder<> builder;
+    std::unique_ptr<llvm::Module> module;
+    std::map<std::string, llvm::Constant* > names_2_constants;
+    std::vector<CodeBlock* > codeblock_list;
+
+};
+
+
+class Generator{
+    public:
+        Generator() = default;
+        ~Generator();
+
+};
