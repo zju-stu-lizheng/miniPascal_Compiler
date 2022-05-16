1e6430b2055c98543e3689e1f4c63403e152b1e3
Alice-6161 <3190102199@zju.edu.cn>
2022-05-16 23:46:03 +0800

    update statement

--- test/test.pas
+++ test/test.pas
@@ -4,5 +4,16 @@ Const
     constTerm2 = 3.14;
 var
     curTerm1 :integer;
+    curTerm2 :integer;
 begin
+    curTerm1 := constTerm1;
+    curTerm1 := 1 + 2*(2 + 3);
+    
+    if curTerm1 = 11 then
+        curTerm2 := 5
+    else
+        if curTerm1 = 12 then
+            curTerm2 := 6
+        else
+            curTerm2 := 7;
 end.
